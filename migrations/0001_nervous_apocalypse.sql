1d739b70d919636a380e66b5b4aa9afffb18508a
Vinicius <vinicius.steigleder@gmail.com>
2026-01-12 13:32:42 +0100

    chore: sync db migrations

---
+++ migrations/0001_nervous_apocalypse.sql
@@ -0,0 +1,3 @@
+ALTER TABLE "transactions" ADD COLUMN "matched_keyword" text;--> statement-breakpoint
+ALTER TABLE "transactions" ADD COLUMN "app_category_id" varchar;--> statement-breakpoint
+ALTER TABLE "transactions" ADD COLUMN "app_category_name" text;
