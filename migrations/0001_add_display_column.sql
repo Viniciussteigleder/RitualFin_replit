158b886127880a5fc7af49b15f8fef2f25d3b39e
Vinicius <vinicius.steigleder@gmail.com>
2026-01-12 13:34:58 +0100

    update

--- migrations/0001_add_display_column.sql
+++
@@ -1,5 +0,0 @@
--- Add display column to transactions table
-ALTER TABLE transactions ADD COLUMN IF NOT EXISTS display text DEFAULT 'yes' NOT NULL;
-
--- Add conflict_flag column to transactions table
-ALTER TABLE transactions ADD COLUMN IF NOT EXISTS conflict_flag boolean DEFAULT false NOT NULL;
